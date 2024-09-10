e2653f45697198b600092e96c334e8f4a0564e33
Caleb Brewer <caleb@cazzer.io>
2024-09-10 11:53:32 -0400

    resolving base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import commonjs from '@rollup/plugin-commonjs'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
+  base: 'tides',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
